3f359daae600ffe379a573185be58a86de48c4b8
Quinn Bowers <q.bowers@outlook.com>
2017-07-31 23:09:17 -0700

    makes new_admin the html file in use

--- main.py
+++ main.py
@@ -183,7 +183,8 @@ class AdminHandler(BaseHandler):
             row = JINJA_ENVIRONMENT.get_template('public/studentrow.html')
             rows += row.render(self.db_user_to_simple_obj(student))
 
-        template = JINJA_ENVIRONMENT.get_template('public/admin.html')
+        ##Replace new_admin.html with admin.html for practical purposes
+        template = JINJA_ENVIRONMENT.get_template('public/new_admin.html')
 
         self.response.write(template.render({'students' : rows}))
 
